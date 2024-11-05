e6f738b6859d753b631916290d7a46f04dd10c4d
Abdul Shakoor Khan <abdulshakoorkhan475@gmail.com>
2024-11-05 09:40:27 +0500

    Add comment to the script.js and projects.js

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,30 +1,37 @@
+// Export an array of 'projects', Here each project is an object with image, heading, and description.
 export let projects = [
   {
+    // Project 1 details
     image: "images/laptop.png",
     caption_heading: "Ecommerce Website",
     caption_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam itaque voluptate quisquam dolores esse"
   },
   {
+    // Project 2 details
     image: "images/laptop.png",
     caption_heading: "Ecommerce Website",
     caption_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam itaque voluptate quisquam dolores esse"
   },
   {
+    // Project 3 details
     image: "images/laptop.png",
     caption_heading: "Ecommerce Website",
     caption_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam itaque voluptate quisquam dolores esse"
   },
   {
+    // Project 4 details
     image: "images/laptop.png",
     caption_heading: "Ecommerce Website",
     caption_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam itaque voluptate quisquam dolores esse"
   },
   {
+    // Project 5 details
     image: "images/laptop.png",
     caption_heading: "Ecommerce Website",
     caption_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam itaque voluptate quisquam dolores esse"
   },
   {
+    // Project 6 details
     image: "images/laptop.png",
     caption_heading: "Ecommerce Website",
     caption_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam itaque voluptate quisquam dolores esse"
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,22 @@
+// Import the 'projects' array from the projects.js file.
 import { projects } from "./projects.js";
 
+// Wait for the DOM to be fully loaded before executing any scripts.
 document.addEventListener("DOMContentLoaded", () => {
+  // Here we Call the function to display the portfolio projects.
   portfolioProjects();
 });
 
+// Empty string to store the HTML content of the projects.
 let portfolioProjectsHTML = "";
+
+// Function to loop through each project in the 'projects' array and generate HTML.
+
 function portfolioProjects() {
+  // Iterate over each project object in the 'projects' array.
   projects.forEach(project => {
 
+    // HTML structure for each project using template literals and project data.
     portfolioProjectsHTML += `
     <a href="#" class="portfolio-project">
           <div class="port-div">
@@ -23,11 +32,11 @@ function portfolioProjects() {
         </a>
     `
   });
+  // HTML content into the element with the class 'js-portfolio-projects'.
   document.querySelector(".js-portfolio-projects").innerHTML = portfolioProjectsHTML;
 }
 
-
-
+// Swiper carousel for portfolio images.
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
